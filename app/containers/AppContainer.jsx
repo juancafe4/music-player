05e9460e09e89ad21ee01c662cdbc3abf5263ef2
Juan Carlos <juancafe2@gmail.com>
2016-12-25 14:47:38 -0400

    Sound component

--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios'
-
+// Sound component
+import Sound from 'react-sound';
 class AppContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +12,14 @@ class AppContainer extends React.Component {
         track: {stream_url: '', title: '', artwork_url: ''}
     }
     this.randomTrack = this.randomTrack.bind(this);
+    this.prepareUrl = this.prepareUrl.bind(this);
   }
   componentDidMount() {
     this.randomTrack();
+  }
+  prepareUrl(url) {
+    // Attach client id to stream url
+    return `${url}?client_id=${this.client_id}`
   }
   randomTrack() {
     const URL = `https://api.soundcloud.com/playlists/209262931?client_id=${this.client_id}`;
@@ -32,7 +38,13 @@ class AppContainer extends React.Component {
   render() {
     return (
       <div className="scotch_music">
-        
+        <Sound 
+          url={this.prepareUrl(this.state.track.stream_url)}
+          playStatus={this.state.playStatus}
+          onPlaying={this.handleSongPlaying.bind(this)}
+          playFromPosition={this.state.playFromPosition}
+          onFinishedPlaying={this.handleSongFinished.bind(this)}
+        />
       </div>
     );
   }
